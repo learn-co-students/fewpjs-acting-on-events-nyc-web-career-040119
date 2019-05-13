259c5331f8a75bbadb88882195d45054b6c42b98
Edgar Ong <emailedgar@gmail.com>
2019-05-13 18:02:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+// HELPER FUNCTIONS
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  // breakpoint is 360 for the right edge
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// EVENT LISTENERS
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+});
